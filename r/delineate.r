5711a225320694ae029decdf6f887efaeb0f6c69
Matt <matthew.talluto@uibk.ac.at>
2022-07-20 13:57:27 +0200

    change to delineation to capture all study sites

--- r/delineate.r
+++ r/delineate.r
@@ -6,7 +6,7 @@ dem = rast("../ybbs_rn/data/dhm_at_lamb_10m_2018_epsg3035.tif")
 ext = c(4670000, 4710000, 2810000, 2860000)
 dem = crop(dem, ext)
 outlet = c(4704445, 2847453)
-kamp = delineate(raster(dem), threshold = 3e6, outlet = outlet, reach_len = 500)
+kamp = delineate(raster(dem), threshold = 1.2e6, outlet = outlet, reach_len = 500)
 
 writeRaster(kamp, file="output/kamp.grd", overwrite = TRUE, gdal=c("COMPRESS=DEFLATE"))
 
